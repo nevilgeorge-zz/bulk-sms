e1cba4d7f3315f648ec988f0c1f2b0d8850fff64
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-12-19 23:02:52 -0600

    adding basic views

--- app/views.py
+++ app/views.py
@@ -1,13 +1,36 @@
 # views.py
 from app import app
-from flask import render_template
+from flask import flash, render_template
+from forms import SendMessageForm
 
 @app.route('/')
 @app.route('/index')
 def index():
+	"""Render index page."""
 	user = {'name': 'Nevil'}
 	return render_template(
 		'index.html',
 		hackathon='Wildhacks',
 		user=user
+	)
+
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+	"""Render sms sending page."""
+	send_message_form = SendMessageForm()
+	if send_message_form.validate_on_submit():
+		flash('Message sending!')
+
+	return render_template(
+		'send.html',
+		form=send_message_form
+	)
+
+
+@app.route('/add')
+def add():
+	"""Render add number/ add by file page."""
+	return render_template(
+		'add.html'
 	)
