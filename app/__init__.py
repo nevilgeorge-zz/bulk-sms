afa4044585b176ca5a934887611527430ab95004
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-12-30 15:02:30 -0600

    configuring celery and adding dependency to requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
+from celery import Celery
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+# configure celery
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 from app import views, models
